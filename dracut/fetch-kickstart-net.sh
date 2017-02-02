aab9852c521358c2dd95e4ce974900dfa4b05212
Vendula Poncova <vponcova@redhat.com>
2017-02-02 17:34:30 +0100

    Enable multiple inst.ks options

--- dracut/fetch-kickstart-net.sh
+++ dracut/fetch-kickstart-net.sh
@@ -5,58 +5,68 @@
 # initqueue/online hook passes interface name as $1
 netif="$1"
 
-# we already processed the kickstart - exit
+# We already processed the kickstart - exit.
 [ -e /tmp/ks.cfg.done ] && return 0
 
-# no kickstart requested - exit
-[ -n "$kickstart" ] || return 0
+# No network kickstart requested - exit.
+[ -e /tmp/ks_net ] || return 0
 
-# user requested a specific device, but this isn't it - exit
+# User requested a specific device, but this isn't it - exit.
 [ -n "$ksdevice" ] && [ "$ksdevice" != "$netif" ] && return 0
 
 command -v getarg >/dev/null || . /lib/dracut-lib.sh
 . /lib/url-lib.sh
 . /lib/anaconda-lib.sh
 
-if [ "$kickstart" = "nfs:auto" ]; then
-    # construct kickstart URL from dhcp info
-    # server is next_server, or the dhcp server itself if missing
-    . /tmp/net.$netif.dhcpopts
-    server="${new_next_server:-$new_dhcp_server_identifier}"
-    # filename is dhcp 'filename' option, or '/kickstart/' if missing
-    filename="/kickstart/"
-    # read the dhcp lease file and see if we can find 'filename'
-    { while read line; do
-        val="${line#filename }"
-        if [ "$val" != "$line" ]; then
-            eval "filename=$val" # drop quoting and semicolon
-        fi
-      done
-    } < /tmp/net.$netif.lease
-    kickstart="nfs:$server:$filename"
-fi
+# Try to get one of the network files.
+for kickstart in $(cat /tmp/ks_net); do
 
-# NFS kickstart URLs that end in '/' get '$IP_ADDR-kickstart' appended
-case "$kickstart" in
-    nfs*/) kickstart="${kickstart}${new_ip_address}-kickstart" ;;
-esac
+    # NFS auto
+    if [ "$kickstart" = "nfs:auto" ]; then
+        # Server is next_server, or the dhcp server itself if missing.
+        . /tmp/net.$netif.dhcpopts
+        server="${new_next_server:-$new_dhcp_server_identifier}"
 
-# If we're doing sendmac, we need to run after anaconda-ks-sendheaders.sh
-if getargbool 0 inst.ks.sendmac kssendmac; then
-    newjob=$hookdir/initqueue/settled/fetch-ks-${netif}.sh
-else
-    newjob=$hookdir/initqueue/fetch-ks-${netif}.sh
-fi
+        # Filename is dhcp 'filename' option, or '/kickstart/' if missing.
+        filename="/kickstart/"
 
-cat > $newjob <<__EOT__
-. /lib/url-lib.sh
-. /lib/anaconda-lib.sh
-info "anaconda fetching kickstart from $kickstart"
-if fetch_url "$kickstart" /tmp/ks.cfg; then
-    parse_kickstart /tmp/ks.cfg
-    run_kickstart
-else
-    warn "failed to fetch kickstart from $kickstart"
-fi
-rm \$job # remove self from initqueue
-__EOT__
+        # Read the dhcp lease file and see if we can find 'filename'.
+        { while read line; do
+            val="${line#filename }"
+            if [ "$val" != "$line" ]; then
+                eval "filename=$val" # drop quoting and semicolon
+            fi
+          done
+        } < /tmp/net.$netif.lease
+
+        # Construct kickstart URL from dhcp info.
+        kickstart="nfs:$server:$filename"
+    fi
+
+    # NFS
+    case "$kickstart" in
+        # NFS kickstart URLs that end in '/' get '$IP_ADDR-kickstart' appended
+        nfs*/) kickstart="${kickstart}${new_ip_address}-kickstart" ;;
+    esac
+
+    # If we're doing sendmac, we need to run after anaconda-ks-sendheaders.sh
+    if getargbool 0 inst.ks.sendmac kssendmac; then
+        newjob=$hookdir/initqueue/settled/fetch-ks-${netif}.sh
+    else
+        newjob=$hookdir/initqueue/fetch-ks-${netif}.sh
+    fi
+
+    cat > $newjob <<__EOT__
+    . /lib/url-lib.sh
+    . /lib/anaconda-lib.sh
+    info "anaconda fetching kickstart from $kickstart"
+    if fetch_url "$kickstart" /tmp/ks.cfg; then
+        parse_kickstart /tmp/ks.cfg
+        run_kickstart
+        break
+    else
+        warn "failed to fetch kickstart from $kickstart"
+    fi
+    rm \$job # remove self from initqueue
+    __EOT__
+done
--- dracut/parse-anaconda-kickstart.sh
+++ dracut/parse-anaconda-kickstart.sh
@@ -1,33 +1,72 @@
 #!/bin/bash
 # parse-anaconda-kickstart.sh: handle kickstart settings
 
-# no need to do this twice
-[ -f /tmp/ks.cfg.done ] && return
+# No need to do this twice.
+[ -e /tmp/ks.cfg.done ] && return
 
-# inst.ks: provide a "URI" for the kickstart file
-kickstart="$(getarg ks= inst.ks=)"
-if [ -z "$kickstart" ]; then
-    getargbool 0 ks inst.ks && kickstart='nfs:auto'
-fi
-# no root? the kickstart will probably tell us what our root device is.
-[ "$kickstart" ] && [ -z "$root" ] && root="anaconda-kickstart"
+# Clear everything.
+rm -f /tmp/ks_net /tmp/ks_local
 
-case "${kickstart%%:*}" in
-    http|https|ftp|nfs|nfs4) # network kickstart? set "neednet"!
-        set_neednet
-    ;;
-    file|path) # "file:<path>" - "path:<path>" is accepted but deprecated
+# Process all inst.ks arguments.
+# inst.ks provides an "URI" for the kickstart file
+for kickstart in $(getargs ks= inst.ks=); do
+
+    if [ -z "$kickstart" ]; then
+        getargbool 0 ks inst.ks && kickstart='nfs:auto'
+    fi
+
+    case "${kickstart%%:*}" in
+        http|https|ftp|nfs|nfs4)
+            # Network kickstart files.
+            echo $kickstart >> /tmp/ks_net
+        ;;
+        file|path)
+            # Local kickstart files.
+            echo $kickstart >> /tmp/ks_local
+        ;;
+    esac
+done
+
+# No root? The kickstart file will probably tell us what our root device is.
+[ -e /tmp/ks_local -o -e /tmp/ks_net ] && [ -z "$root" ] && root="anaconda-kickstart"
+
+# Try to get one of the local files.
+if [ -f /tmp/ks_local ]; then
+
+    for kickstart in $(cat /tmp/ks_local); do
+        # Split the argument.
+        # file:<path> or path:<path> (accepted but deprecated)
         splitsep ":" "$kickstart" kstype kspath
+
+        # Does the file exist?
         if [ -f "$kspath" ]; then
+            # Parse the file.
             info "anaconda: parsing kickstart $kspath"
             cp $kspath /tmp/ks.cfg
             parse_kickstart /tmp/ks.cfg
             [ "$root" = "anaconda-kickstart" ] && root=""
             > /tmp/ks.cfg.done
+
+            # Export the kickstart variable and return.
+            export kickstart
+            return
         else
+            # Show warnings and continue.
             warn "inst.ks='$kickstart'"
             warn "can't find $kspath!"
         fi
-    ;;
-esac
+    done
+fi
+
+# If no local file was found, we will try the network files later.
+if [ -f /tmp/ks_net ]; then
+
+    # Set the variable to the first file on the list.
+    kickstart=$(head -n 1 /tmp/ks_net)
+
+    # We need the network.
+    set_neednet
+fi
+
+# Export the kickstart variable and return.
 export kickstart
