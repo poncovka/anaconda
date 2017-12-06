b4655c12b6a9102551717e7e22a2a1a1c95c6b50
Vendula Poncova <vponcova@redhat.com>
2017-12-06 13:01:46 +0100

    Fix

--- pyanaconda/installclasses/fedora-workstation.py
+++ pyanaconda/installclasses/fedora-workstation.py
@@ -1,6 +1,12 @@
 from pyanaconda.installclasses.fedora import FedoraBaseInstallClass
+from pyanaconda.product import productName
+
 
 class FedoraWorkstationInstallClass(FedoraBaseInstallClass):
     name = "Fedora Workstation"
     stylesheet = "/usr/share/anaconda/pixmaps/workstation/fedora-workstation.css"
     defaultPackageEnvironment = "workstation-product-environment"
+
+    sortPriority = FedoraBaseInstallClass.sortPriority + 1
+    if not productName.startswith("Fedora Workstation"):  # pylint: disable=no-member
+        hidden = True
